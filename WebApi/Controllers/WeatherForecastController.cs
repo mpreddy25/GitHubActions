98daab5d4f7ab29c32db33e9b07e9bcdd8fdafc9
Prasanth Reddy M <prasanthreddy.maddala@outlook.com>
2024-03-11 20:45:31 +0530

    making api changes to test

--- WebApi/Controllers/WeatherForecastController.cs
+++ WebApi/Controllers/WeatherForecastController.cs
@@ -21,13 +21,14 @@ namespace WebApi.Controllers
         [HttpGet]
         public IEnumerable<WeatherForecast> Get()
         {
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                TemperatureC = Random.Shared.Next(-20, 55),
-                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-            })
-            .ToArray();
+            return new List<WeatherForecast>();
+            //return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            //{
+            //    Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            //    TemperatureC = Random.Shared.Next(-20, 55),
+            //    Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            //})
+            //.ToArray();
         }
     }
 }
